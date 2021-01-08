e83e6775b9a33ddfb04849c7d31841356371350e
Bastian Böhm <contact.bastianboehm@gmail.com>
2021-01-08 18:31:33 +0100

    Skeleton for handling different hosts

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const {spawn} = require('child_process');
 const _ = require('lodash');
 
-// TODO Pay attention to differences in argument passing styles between operating systems
+// Position for HTML5 video & audio: #t=
+// Position for PDF: #page=
 
 const availableBrowsers = getAvailableBrowsers();
 if (_.isEmpty(availableBrowsers)) {
@@ -12,17 +13,33 @@ if (_.isEmpty(availableBrowsers)) {
 }
 const browser = Object.keys(availableBrowsers)[0];
 
-// TODO Google Drive sharing links
-// Frei verfügbar: https://drive.google.com/file/d/1sn3Ajj7pY26XKOrSpnDQvK0N5TqF8yMo/view?usp=sharing
-// Nur für hinzugefügte Personen: https://drive.google.com/file/d/1Tx8iULXcrjejgKluKukE4AdS3Eoe8uNT/view?usp=sharing
-
 router.post('/open', function (req, res) {
+    // TODO Differences in argument passing between operating systems?
     spawn(browser, [resolveTargetToURL(req.body)]);
     res.status(200).send();
 });
 
+// Check content-type with HTTP HEAD request
+// Download & display via file:// URL
+// Simply open text/html or unknown content-types
 function resolveTargetToURL(target) {
-    return `${target.URL}?t=${target.position}`;
+    // YouTube link
+    if (target.URL.match(/^(http[s]?:\/\/)?(www\.)?youtube\.com/)) {
+        return `${target.URL}&t=${target.position}`;
+    }
+    // Google Drive (sharing) link
+    else if (target.URL.match(/^(http[s]?:\/\/)?(www\.)?drive.google.com\/file\/d\//)) {
+        // TODO Get webContentLink via Google Drive API
+        // https://drive.google.com/file/d/1sn3Ajj7pY26XKOrSpnDQvK0N5TqF8yMo/view?usp=sharing - public
+        // https://drive.google.com/file/d/1Tx8iULXcrjejgKluKukE4AdS3Eoe8uNT/view?usp=sharing - restricted
+    } else {
+        return `${target.URL}${target.position}`;
+    }
+}
+
+// Receives content-type via HTTP HEAD request
+function getContentType() {
+
 }
 
 module.exports = router;
